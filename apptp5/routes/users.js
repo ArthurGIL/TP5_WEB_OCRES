2c769f5a25fa927732d9324aa6a49081c79085ce
ArthurGIL <49671280+ArthurGIL@users.noreply.github.com>
2021-11-10 15:21:32 +0100

    lodash

--- apptp5/routes/users.js
+++ apptp5/routes/users.js
@@ -1,3 +1,20 @@
+// Load the full build.
+var _ = require('lodash');
+// Load the core build.
+var _ = require('lodash/core');
+// Load the FP build for immutable auto-curried iteratee-first data-last methods.
+var fp = require('lodash/fp');
+ 
+// Load method categories.
+var array = require('lodash/array');
+var object = require('lodash/fp/object');
+ 
+// Cherry-pick methods for smaller browserify/rollup/webpack bundles.
+var at = require('lodash/at');
+var curryN = require('lodash/fp/curryN');
+
+/*-----------------------------------------------------------------------------------*/
+
 var express = require('express');
 var router = express.Router();
 
@@ -5,5 +22,20 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* POST users listing. */
+/*
+router.post('/:id', (req, res) => {
+  const {id} = req.params;
+  const {user} = req.body;
+  const userToUpdate = _.find(users, ["id", id]);
+
+  userToUpdate.user = user;
+
+  res.json({
+    message: 'Just updated ${id} with ${user}'
+  });
+});
+*/
 
 module.exports = router;
